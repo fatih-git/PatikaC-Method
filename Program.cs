b01f118e5b2a73d3dfd670b431d5ce5f52edbb5b
fatih-git <72505641+fatih-git@users.noreply.github.com>
2022-12-07 15:58:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Deneme2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 3;
+            int b = 4;
+            int sonuc=0;
+            sonuc = ArttırveTopla(ref a, ref b);
+
+        }
+
+        public int ArttırveTopla(ref int x, ref int y)
+        {
+            x++;
+            y += y;
+
+            return (x+y);
+        }
+
+    }
+}
